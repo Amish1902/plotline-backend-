cc2bf9189ba72977eaaf11384c7325c5c717142e
Amish Mishra <72759107+Amish1902@users.noreply.github.com>
2023-07-31 20:41:16 +0530

    Create serviceController.test.js

---
+++ setting enviroment/tests/serviceController.test.js
@@ -0,0 +1,6 @@
+const request = require('supertest');
+const app = require('../app');
+const mongoose = require('mongoose');
+const Service = require('../models/service');
+
+// Add your service-related test cases here
