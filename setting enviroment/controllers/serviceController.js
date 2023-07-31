565596c70523743f0dbfc05076abd8f7a0b6f536
Amish Mishra <72759107+Amish1902@users.noreply.github.com>
2023-07-31 20:39:53 +0530

    Create serviceController.js

---
+++ setting enviroment/controllers/serviceController.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const Service = require('../models/service');
+
+// Add your service-related endpoints here
+
+module.exports = router;
