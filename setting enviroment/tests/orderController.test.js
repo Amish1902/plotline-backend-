91594954a9de96c0e025f4707bc3f5f472af4756
Amish Mishra <72759107+Amish1902@users.noreply.github.com>
2023-07-31 20:41:45 +0530

    Create orderController.test.js

---
+++ setting enviroment/tests/orderController.test.js
@@ -0,0 +1,6 @@
+const request = require('supertest');
+const app = require('../app');
+const mongoose = require('mongoose');
+const Order = require('../models/order');
+
+// Add your order-related test cases here
