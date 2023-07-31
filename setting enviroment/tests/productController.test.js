79a874d8df18055188d9846295149fbe9a78cb4b
Amish Mishra <72759107+Amish1902@users.noreply.github.com>
2023-07-31 20:41:01 +0530

    Create productController.test.js

---
+++ setting enviroment/tests/productController.test.js
@@ -0,0 +1,6 @@
+const request = require('supertest');
+const app = require('../app');
+const mongoose = require('mongoose');
+const Product = require('../models/product');
+
+// Add your product-related test cases here
