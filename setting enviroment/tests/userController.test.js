0cfad1092458e997db6ed5a83a4ce2041fdc8ad8
Amish Mishra <72759107+Amish1902@users.noreply.github.com>
2023-07-31 20:40:43 +0530

    Create userController.test.js

---
+++ setting enviroment/tests/userController.test.js
@@ -0,0 +1,6 @@
+const request = require('supertest');
+const app = require('../app');
+const mongoose = require('mongoose');
+const User = require('../models/user');
+
+// Add your user-related test cases here
